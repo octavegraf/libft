575a553660a3bc516e9be854e5f1109c429d96ca
Octave Graf <cobra.morilles-0n@icloud.com>
2025-03-04 18:45:40 +0100

    added function

--- libft.h
+++ libft.h
@@ -6,11 +6,27 @@
 /*   By: ocgraf <ocgraf@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/03 10:09:54 by ocgraf            #+#    #+#             */
-/*   Updated: 2025/03/04 13:17:27 by ocgraf           ###   ########.fr       */
+/*   Updated: 2025/03/04 18:45:24 by ocgraf           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 # include <stdlib.h>
+
+int		ft_isalnum(int c);
+int		ft_isalpha(int c);
+int		ft_isascii(int c);
+int		ft_isdigit(int c);
+int		ft_isprint(int c);
+size_t	ft_strlen(const char *s);
+size_t	ft_strlcat(char *restrict dst, const char *restrict src,
+			size_t dstsize);
+size_t	ft_strlcpy(char *restrict dst, const char *restrict src,
+			size_t dstsize);
+void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
+void	*ft_memmove(void *dst, const void *src, size_t len);
+void	*ft_memset(void *b, int c, size_t len);
+void	ft_bzero(void *s, size_t n);
+
 #endif
