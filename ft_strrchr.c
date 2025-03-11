badec23623d94d28b8df41fc6867e83da074bb8c
Octave Graf <cobra.morilles-0n@icloud.com>
2025-03-11 13:03:22 +0100

    improved logic

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: ocgraf <ocgraf@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/04 18:49:40 by ocgraf            #+#    #+#             */
-/*   Updated: 2025/03/11 11:10:19 by ocgraf           ###   ########.fr       */
+/*   Updated: 2025/03/11 13:01:38 by ocgraf           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,11 @@ char	*ft_strrchr(const char *s, int c)
 	size_t	i;
 
 	str = (char *)s;
-	i = ft_strlen(str) - 1;
-	while (i >= 0)
+	i = ft_strlen(str);
+	while (i > 0)
 	{
-		if (str[i] == c)
+		if (str[--i] == c)
 			return (&str[i]);
-		i--;
 	}
 	return (NULL);
 }
