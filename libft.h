7c5f3cfd2d86b9f24f27ca4a8f4ed61208eb8941
Octave Graf <cobra.morilles-0n@icloud.com>
2025-03-11 11:10:36 +0100

    added function

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ocgraf <ocgraf@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/03 10:09:54 by ocgraf            #+#    #+#             */
-/*   Updated: 2025/03/04 19:01:59 by ocgraf           ###   ########.fr       */
+/*   Updated: 2025/03/11 11:04:48 by ocgraf           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,8 @@ size_t	ft_strlcat(char *restrict dst, const char *restrict src,
 			size_t dstsize);
 size_t	ft_strlcpy(char *restrict dst, const char *restrict src,
 			size_t dstsize);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strrchr(const char *s, int c);
 void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
 void	*ft_memset(void *b, int c, size_t len);
