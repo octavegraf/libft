f22bac1e66bf596b84edebef56c6dba14e3326d4
Octave Graf <cobra.morilles-0n@icloud.com>
2025-03-13 11:48:15 +0100

    added function

---
+++ ft_substr.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ocgraf <ocgraf@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/12 14:24:17 by ocgraf            #+#    #+#             */
+/*   Updated: 2025/03/13 11:47:59 by ocgraf           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	unsigned int			i;
+	char					*subs;
+
+	subs = malloc((len + 1) * sizeof(char));
+	if (!subs)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		subs[i] = s[start + i];
+		i++;
+	}
+	subs[i] = '\0';
+	return (subs);
+}
