af4cbd6d92d7e4d76d75d3f2e6e131adde03e116
Octave Graf <cobra.morilles-0n@icloud.com>
2025-03-29 09:29:09 +0100

    added condition if *lst is NULL

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: ocgraf <ocgraf@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/18 21:03:58 by ocgraf            #+#    #+#             */
-/*   Updated: 2025/03/18 21:43:50 by ocgraf           ###   ########.fr       */
+/*   Updated: 2025/03/29 09:28:49 by ocgraf           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,11 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 {
 	t_list	*temp;
 
-	temp = ft_lstlast(*lst);
-	temp->next = new;
+	if (*lst == NULL)
+		ft_lstadd_front(lst, new);
+	else
+	{
+		temp = ft_lstlast(*lst);
+		temp->next = new;
+	}
 }
