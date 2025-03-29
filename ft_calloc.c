51a9b29d3607dac5bcb5cd567cadfcb7df010688
Octave Graf <cobra.morilles-0n@icloud.com>
2025-03-29 09:22:49 +0100

    removed unused condition

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: ocgraf <ocgraf@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/12 10:35:16 by ocgraf            #+#    #+#             */
-/*   Updated: 2025/03/12 13:09:42 by ocgraf           ###   ########.fr       */
+/*   Updated: 2025/03/29 09:22:26 by ocgraf           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,16 @@
 
 int	detect_overflow(size_t count, size_t size)
 {
-	if (count > 0 && size > 0 && count > INT_MAX / size)
+	if (size > 0 && count > INT_MAX / size)
 	{
 		return (1);
 	}
-	if (count < 0 && size < 0 && count < INT_MAX / size)
+	if (size < 0 && count < INT_MAX / size)
 	{
 		return (1);
 	}
-	if ((count > 0 && size < 0 && size < INT_MIN / count)
-		|| (count < 0 && size > 0 && count < INT_MIN / size))
+	if ((size < 0 && size < INT_MIN / count)
+		|| (size > 0 && count < INT_MIN / size))
 	{
 		return (1);
 	}
