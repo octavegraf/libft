0fca4f0294b762318cd0768af6f867817017db4b
Octave Graf <ocgraf@bess-f3r5s8.clusters.42paris.fr>
2025-04-22 18:02:35 +0200

    corrected wrong function declaration

--- src/libft.h
+++ src/libft.h
@@ -6,7 +6,7 @@
 /*   By: ocgraf <ocgraf@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/03 10:09:54 by ocgraf            #+#    #+#             */
-/*   Updated: 2025/04/22 17:41:31 by ocgraf           ###   ########.fr       */
+/*   Updated: 2025/04/22 17:58:28 by ocgraf           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,7 +46,7 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 char	*ft_strrchr(const char *s, int c);
-int		ft_check(const char *haystack, const char *needle);
+int		ft_check(const char *haystack, const char *needle, size_t len);
 char	*ft_strtrim(char const *s1, char const *set);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 int		ft_tolower(int c);
