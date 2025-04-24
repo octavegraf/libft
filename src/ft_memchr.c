3011122c4cc87dfbfeb26af1543f10fd38cbc7e0
Octave Graf <ocgraf@student.42.fr>
2025-04-24 12:47:10 +0200

    changed to continue to explore memory, even if str is ended

--- src/ft_memchr.c
+++ src/ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: ocgraf <ocgraf@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/11 15:11:15 by ocgraf            #+#    #+#             */
-/*   Updated: 2025/04/22 17:57:54 by ocgraf           ###   ########.fr       */
+/*   Updated: 2025/04/24 12:45:21 by ocgraf           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,10 +23,10 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	i = 0;
 	uc = (unsigned char)c;
 	str = s;
-	while (str[i] && i < n)
+	while (i < n)
 	{
 		if (str[i] == uc)
-			return ((unsigned char *)(s + i));
+			return ((void *)(s + i));
 		i++;
 	}
 	return (NULL);
