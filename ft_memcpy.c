72bdefd5542d00a2ce7b349c0daaf27b42f125c4
Octave Graf <ocgraf@student.42.fr>
2025-04-25 16:45:52 +0200

    addded protection

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: ocgraf <ocgraf@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/03 09:54:53 by ocgraf            #+#    #+#             */
-/*   Updated: 2025/04/22 17:43:03 by ocgraf           ###   ########.fr       */
+/*   Updated: 2025/04/25 16:43:15 by ocgraf           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	char	*dest;
 	char	*source;
 
+	if (!src && !dst)
+		return (NULL);
 	dest = (char *)dst;
 	source = (char *)src;
 	i = 0;
@@ -28,18 +30,3 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	}
 	return (dst);
 }
-/* #include <string.h>
-#include <stdio.h>
- 
-#define MAX_LEN 80
- 
-char source[ MAX_LEN ] = "This is the source string";
-char target[ MAX_LEN ] = "This is the target string";
- 
-int main(void)
-{
-  printf( "Before memcpy, target is \"%s\"\n", target );
-  ft_memcpy( target, source, sizeof(source));
-  printf( "After memcpy, target becomes \"%s\"\n", target );
-}
-Source du main : Microsoft Learn */
