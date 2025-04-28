4661cdc3d21339aae8684c78c9f8657b12425422
Octave Graf <ocgraf@student.42.fr>
2025-04-28 13:33:47 +0200

    modified protection for empty string or start > strlen

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: ocgraf <ocgraf@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/12 14:24:17 by ocgraf            #+#    #+#             */
-/*   Updated: 2025/04/27 17:36:33 by ocgraf           ###   ########.fr       */
+/*   Updated: 2025/04/28 11:02:16 by ocgraf           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,9 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	unsigned int			i;
 	char					*subs;
 
-	if ((start > ft_strlen(s)) || !s)
+	if (!s)
+		return (NULL);
+	else if ((start > ft_strlen(s)))
 		len = 0;
 	subs = malloc((len + 1) * sizeof(char));
 	if (!subs)
