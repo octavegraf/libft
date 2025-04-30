a35213bf32e492ac76ee0be58dcb43caa99949d6
Octave Graf <ocgraf@student.42.fr>
2025-04-30 10:06:56 +0200

    corrected split spearator function

--- ft_split.c
+++ ft_split.c
@@ -6,13 +6,18 @@
 /*   By: ocgraf <ocgraf@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/14 15:27:59 by ocgraf            #+#    #+#             */
-/*   Updated: 2025/04/28 18:28:11 by ocgraf           ###   ########.fr       */
+/*   Updated: 2025/04/30 10:00:25 by ocgraf           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	is_in_charset(char c, char const *set);
+int	c_is_in_charset(char c, char set)
+{
+	if (c == set)
+		return (1);
+	return (0);
+}
 
 void	free_everything(char **tabtab, int k)
 {
@@ -21,7 +26,7 @@ void	free_everything(char **tabtab, int k)
 	free(tabtab);
 }
 
-int	count_words(char const *s, char const *set)
+int	count_words(char const *s, char set)
 {
 	int	i;
 	int	words;
@@ -30,27 +35,27 @@ int	count_words(char const *s, char const *set)
 	words = 0;
 	while (s[i])
 	{
-		if ((((i == 0) || (is_in_charset(s[i - 1], set)))
-				&& (!is_in_charset(s[i], set))))
+		if ((((i == 0) || (c_is_in_charset(s[i - 1], set)))
+				&& (!c_is_in_charset(s[i], set))))
 			words++;
 		i++;
 	}
 	return (words);
 }
 
-char	*cut_words(char const *s, char const *set)
+char	*cut_words(char const *s, char set)
 {
 	int		i;
 	char	*word;
 
 	i = 0;
-	while (s[i] && !is_in_charset(s[i], set))
+	while (s[i] && !c_is_in_charset(s[i], set))
 		i++;
 	word = malloc((i + 1) * sizeof(char));
 	if (!word)
 		return (NULL);
 	i = 0;
-	while (s[i] && !is_in_charset(s[i], set))
+	while (s[i] && !c_is_in_charset(s[i], set))
 	{
 		word[i] = s[i];
 		i++;
@@ -66,7 +71,7 @@ char	**ft_split(char const *s, char c)
 	int		j;
 	int		k;
 
-	j = count_words(s, &c);
+	j = count_words(s, c);
 	tabtab = malloc((j + 1) * sizeof(char *));
 	if (!tabtab)
 		return (NULL);
@@ -74,9 +79,9 @@ char	**ft_split(char const *s, char c)
 	k = 0;
 	while (k < j)
 	{
-		while (s[i] && is_in_charset(s[i], &c))
+		while (s[i] && c_is_in_charset(s[i], c))
 			i++;
-		tabtab[k] = cut_words(s + i, &c);
+		tabtab[k] = cut_words(s + i, c);
 		if (!tabtab[k])
 			return (free_everything(tabtab, k), NULL);
 		i += ft_strlen(tabtab[k]);
