fa9e3b36973437eb3f7cd8f3e09b4299b1fa2b29
Octave Graf <ocgraf@student.42.fr>
2025-05-30 17:13:04 +0200

    norme

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: ocgraf <ocgraf@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/12 14:24:17 by ocgraf            #+#    #+#             */
-/*   Updated: 2025/03/13 11:47:59 by ocgraf           ###   ########.fr       */
+/*   Updated: 2025/05/30 16:35:23 by ocgraf           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,6 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 		return (ft_strdup(""));
 	if (len > size_len - start)
 		len = size_len - start;
-
 	subs = malloc((len + 1) * sizeof(char));
 	if (!subs)
 		return (NULL);
