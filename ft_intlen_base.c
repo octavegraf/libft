923a2b3bcb90a628c47f02cbdffa1a77b7431b7c
Octave Graf <ocgraf@student.42.fr>
2025-06-02 15:14:02 +0200

    using unsigned long long int instead of unsigned int

--- ft_intlen_base.c
+++ ft_intlen_base.c
@@ -6,27 +6,22 @@
 /*   By: ocgraf <ocgraf@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/25 15:38:44 by ocgraf            #+#    #+#             */
-/*   Updated: 2025/05/28 15:46:06 by ocgraf           ###   ########.fr       */
+/*   Updated: 2025/06/02 15:02:17 by ocgraf           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_intlen_base(int nb, char *base)
+size_t	ft_intlen_base(unsigned long long int nb, char *base)
 {
 	size_t				i;
 	size_t				base_len;
-	unsigned long int	ul_long_nb;
 
 	i = 1;
 	base_len = ft_strlen(base);
-	if (nb < 0)
-		ul_long_nb = -(long)nb;
-	else
-		ul_long_nb = nb;
-	while (ul_long_nb >= base_len)
+	while (nb >= base_len)
 	{
-		ul_long_nb /= base_len;
+		nb /= base_len;
 		i++;
 	}
 	return (i);
